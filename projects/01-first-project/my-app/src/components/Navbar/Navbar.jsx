9540f914b470f1ef7d207ad987af5c27b4ddbb87
Artem Karpukhin <a.karpukhin9@gmail.com>
2022-02-17 21:57:26 +0200

    remove .idea folder

--- projects/01-first-project/my-app/src/components/Navbar/Navbar.jsx
+++ projects/01-first-project/my-app/src/components/Navbar/Navbar.jsx
@@ -1,21 +1,25 @@
 import s from './Navbar.module.css';
 
-// let s = {
-//   'nav': 'Navbar_nav__Q704K',
-//   'item': 'Navbar_item__TFAWD'
-// }
-console.log(s);
-
 const Navbar = () => {
-  return (
-    <nav className={s.nav}>
-      <div className={s.item}><a>Profile</a></div>
-      <div className={`${s.item} ${s.active}`}><a>Messages</a></div>
-      <div className={s.item}><a>News</a></div>
-      <div className={s.item}><a>Music</a></div>
-      <div className={s.item}><a>Settings</a></div>
-    </nav>
-  );
+    return (
+        <nav className={s.nav}>
+            <div className={s.item}>
+                <a href="/profile">Profile</a>
+            </div>
+            <div className={`${s.item} ${s.active}`}>
+                <a href="/dialogs">Messages</a>
+            </div>
+            <div className={s.item}>
+                <a>News</a>
+            </div>
+            <div className={s.item}>
+                <a>Music</a>
+            </div>
+            <div className={s.item}>
+                <a>Settings</a>
+            </div>
+        </nav>
+    );
 }
 
 export default Navbar;
