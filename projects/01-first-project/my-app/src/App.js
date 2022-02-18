0149f7cd81fc6a460ef1c6bef94e888a3bc9c2be
Artem Karpukhin <a.karpukhin9@gmail.com>
2022-02-18 11:14:04 +0200

    create some components, add react-router, add notes to lesson 19

--- projects/01-first-project/my-app/src/App.js
+++ projects/01-first-project/my-app/src/App.js
@@ -4,17 +4,28 @@ import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
 import Dialogs from "./components/Dialogs/Dialogs";
+import News from "./components/News/News";
+import Music from "./components/Music/Music";
+import Settings from "./components/Settings/Settings";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 
 
 const App = () => {
-    return (<div className='app-wrapper'>
-        <Header/>
-        <Navbar/>
-        <div className='app-wrapper-content'>
-            <Dialogs/>
-            {/*<Profile />*/}
+    return (<BrowserRouter>
+        <div className='app-wrapper'>
+            <Header/>
+            <Navbar/>
+            <div className='app-wrapper-content'>
+                <Routes>
+                    <Route path='/profile' element={<Profile/>}/>
+                    <Route path='/dialogs' element={<Dialogs/>}/>
+                    <Route path='/news' element={<News/>}/>
+                    <Route path='/music' element={<Music/>}/>
+                    <Route path='/settings' element={<Settings/>}/>
+                </Routes>
+            </div>
         </div>
-    </div>);
+    </BrowserRouter>);
 }
 
 export default App;
--- projects/01-first-project/my-app/src/components/Navbar/Navbar.jsx
+++ projects/01-first-project/my-app/src/components/Navbar/Navbar.jsx
@@ -10,16 +10,15 @@ const Navbar = () => {
                 <a href="/dialogs">Messages</a>
             </div>
             <div className={s.item}>
-                <a>News</a>
+                <a href="/news">News</a>
             </div>
             <div className={s.item}>
-                <a>Music</a>
+                <a href="/music">Music</a>
             </div>
             <div className={s.item}>
-                <a>Settings</a>
+                <a href="/settings">Settings</a>
             </div>
-        </nav>
-    );
+        </nav>);
 }
 
 export default Navbar;
