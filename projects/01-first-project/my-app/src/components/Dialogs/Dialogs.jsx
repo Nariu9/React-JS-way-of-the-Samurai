b4ad6dd75d599d4b6d06a8b0a285d86b147fab5a
Artem Karpukhin <a.karpukhin9@gmail.com>
2022-02-19 19:53:37 +0200

    make up the Dialogs page, add notes to 21 lesson

--- projects/01-first-project/my-app/src/components/Dialogs/Dialogs.jsx
+++ projects/01-first-project/my-app/src/components/Dialogs/Dialogs.jsx
@@ -2,8 +2,32 @@ import s from './Dialogs.module.css'
 
 const Dialogs = () => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog + ' ' + s.active}>
+                    Dimych
+                </div>
+                <div className={s.dialog}>
+                    Andrey
+                </div>
+                <div className={s.dialog}>
+                    Sveta
+                </div>
+                <div className={s.dialog}>
+                    Sasha
+                </div>
+                <div className={s.dialog}>
+                    Victor
+                </div>
+                <div className={s.dialog}>
+                    Valera
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Hi</div>
+                <div className={s.message}>How are you ?</div>
+                <div className={s.message}>Bye</div>
+            </div>
         </div>
     );
 }
