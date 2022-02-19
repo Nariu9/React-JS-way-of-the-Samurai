0114301327dbc0218473b46baa2baa9b9bd23312
Artem Karpukhin <a.karpukhin9@gmail.com>
2022-02-19 21:51:06 +0200

    add routes to Dialogs, add notes to lesson 22

--- projects/01-first-project/my-app/src/components/Dialogs/Dialogs.jsx
+++ projects/01-first-project/my-app/src/components/Dialogs/Dialogs.jsx
@@ -1,26 +1,27 @@
 import s from './Dialogs.module.css'
+import {NavLink} from "react-router-dom";
 
 const Dialogs = () => {
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
                 <div className={s.dialog + ' ' + s.active}>
-                    Dimych
+                      <NavLink to='/dialogs/1'>Dimych</NavLink>
                 </div>
                 <div className={s.dialog}>
-                    Andrey
+                    <NavLink to='/dialogs/2'>Andrey</NavLink>
                 </div>
                 <div className={s.dialog}>
-                    Sveta
+                    <NavLink to='/dialogs/3'>Sveta</NavLink>
                 </div>
                 <div className={s.dialog}>
-                    Sasha
+                    <NavLink to='/dialogs/4'>Sasha</NavLink>
                 </div>
                 <div className={s.dialog}>
-                    Victor
+                    <NavLink to='/dialogs/5'>Victor</NavLink>
                 </div>
                 <div className={s.dialog}>
-                    Valera
+                    <NavLink to='/dialogs/6'>Valera</NavLink>
                 </div>
             </div>
             <div className={s.messages}>
